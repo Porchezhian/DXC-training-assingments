c114fe4fbf763059065ce2864991696ac429ac2b
Porchezhian <samporsche007@gmail.com>
2019-10-17 19:37:11 +0530

    Add files via upload

---
+++ DotNet_Assignment2/DotNet_Assignment2/CustomerAccount.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Assignment2
+{
+    class CustomerAccount
+    { 
+        private int account_number;
+        private string account_holder;
+        private int customer_id;
+        private int account_balance;
+        private int opening_balance;
+        private List<int> deposits = new List<int>();
+        private List<int> withdrawals = new List<int>();
+
+        public CustomerAccount()
+        {
+            this.account_number = 0;
+            this.account_holder = "";
+            this.customer_id = 0;
+            this.account_balance = 0;
+            this.opening_balance = 0;
+        }
+
+        public CustomerAccount(int account_number, string account_holder, int customer_id, int account_balance, int opening_balance)
+        {
+            this.account_number = account_number;
+            this.account_holder = account_holder;
+            this.customer_id = customer_id;
+            this.account_balance = account_balance;
+            this.opening_balance = opening_balance;
+        }
+
+        public int getCustomerId()
+        {
+            return this.customer_id;
+        }
+
+        public void deposit(int amount)
+        {
+            this.account_balance = this.account_balance + amount;
+            this.deposits.Add(amount); 
+            Console.WriteLine("\nDeposited Rs.{0} in your account", amount);
+            Console.WriteLine("Your account balance is Rs.{0}", this.account_balance);
+        }
+
+        public void withdraw(int amount)
+        {
+            if (this.account_balance - 500 < amount)
+            {
+                Console.WriteLine("\nCannot perform the requested operation...\nWithdraw request amount is greater than your maximum withdrawable amount");
+            }
+            else
+            {
+                this.account_balance = this.account_balance - amount;
+                this.withdrawals.Add(amount);
+                Console.WriteLine("\nWithdrew Rs.{0} from your account", amount);
+                Console.WriteLine("Your account balance is Rs.{0}", this.account_balance);
+            }
+        }
+
+        public void showDetails()
+        {
+            Console.WriteLine("\nAccount Holder Name: {0}\nAccount Number: {1}\nAccount Balance: Rs.{2}\nOpening Balance: Rs.{3}", 
+                this.account_holder, this.account_number, this.account_balance, this.opening_balance);
+        }
+
+        public void showDeposits()
+        {
+            if (this.deposits.Count() != 0)
+            {
+                Console.WriteLine("\n");
+                foreach (var item in deposits)
+                {
+                    Console.WriteLine("-Rs.{0}\n", item);
+                }
+            }
+            else
+                Console.WriteLine("\nNot done any deposits yet");
+        }
+
+        public void showWithdrawals()
+        {
+            if (this.withdrawals.Count() != 0)
+            {
+                Console.WriteLine("\n");
+                foreach (var item in withdrawals)
+                {
+                    Console.WriteLine("-Rs.{0}\n", item);
+                }
+            }
+            else
+                Console.WriteLine("\nNot done any withdrawals yet");
+        }
+    }
+}
---
+++ DotNet_Assignment2/DotNet_Assignment2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option;
+            int id;
+            bool set = true;
+            CustomerAccount user = new CustomerAccount();
+            CustomerAccount customer1 = new CustomerAccount(101, "Porsche", 1234, 30000, 1000);
+            CustomerAccount customer2 = new CustomerAccount(102, "Sam", 1235, 20000, 1000);
+            CustomerAccount customer3 = new CustomerAccount(103, "Mike", 1236, 90000, 1000);
+            CustomerAccount customer4 = new CustomerAccount(104, "Jim", 1237, 7000, 1000);
+            CustomerAccount customer5 = new CustomerAccount(105, "Kenny", 1238, 2000, 1000);
+
+            CustomerAccount[] customers = { customer1, customer2, customer3, customer4, customer5};
+            while (true)
+            {
+                while (set)
+                {
+                    Console.WriteLine("Enter your customer Id");
+                    id = int.Parse(Console.ReadLine());
+                    foreach (var customer in customers)
+                    {
+                        if (customer.getCustomerId() == id)
+                        {
+                            user = customer;
+                            set = false;
+                            Console.Clear();
+                            continue;
+                        }
+                    }
+                    if (set == true)
+                        Console.WriteLine("\nCustomer does not exist\n");
+                }
+                set = true;
+                Console.WriteLine("\nWelcome!\n--------\n\nSelect any option to perform");
+                Console.WriteLine("1. Deposit\n2. Withdraw\n3. Show Account Details\n4. Show Recent Deposits\n5. Show Recent Withdrawals\n6. Exit\n");
+                option = int.Parse(Console.ReadLine());
+                while (set)
+                {
+                    switch (option)
+                    {
+                        case 1:
+                            set = false;
+                            Console.WriteLine("\nEnter the amount");
+                            int deposit = int.Parse(Console.ReadLine());
+                            user.deposit(deposit);
+                            break;
+                        case 2:
+                            set = false;
+                            Console.WriteLine("\nEnter the amount");
+                            int withdraw = int.Parse(Console.ReadLine());
+                            user.withdraw(withdraw);
+                            break;
+                        case 3:
+                            user.showDetails();
+                            set = false;
+                            break;
+                        case 4:
+                            user.showDeposits();
+                            set = false;
+                            break;
+                        case 5:
+                            user.showWithdrawals();
+                            set = false;
+                            break;
+                        case 6:
+                            System.Environment.Exit(1);
+                            break;
+                        default: Console.WriteLine("\nInvalid Option"); set = true; break;
+                    }
+                }
+                Console.WriteLine("\nThank You!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
