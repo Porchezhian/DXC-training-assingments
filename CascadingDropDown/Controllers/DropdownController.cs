b048de83e7bd1067c4eed20788e4e0dc6903e9bf
Porchezhian <samporsche007@gmail.com>
2019-11-06 09:55:49 +0530

    Add files via upload

---
+++ CascadingDropDown/Controllers/DropdownController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CascadedDropDown.Models;
+
+namespace CascadedDropDown.Controllers
+{
+    public class DropdownController : Controller
+    {
+        // GET: Dropdown
+        public ActionResult Index()
+        {
+            tblCountry con = new tblCountry();
+            CountriesEntities entity = new CountriesEntities();
+            ViewBag.countries = new SelectList(entity.tblCountries, "country_id", "country");
+            ViewBag.states = new List<SelectListItem>(){ 
+                new SelectListItem{Text="", Value=""} 
+            }; 
+            return View(con);
+        }
+
+        [HttpPost]
+        public ActionResult Index(tblCountry con)
+        {
+            CountriesEntities entity = new CountriesEntities();
+            ViewBag.countries = new SelectList(entity.tblCountries, "country_id", "country");
+            ViewBag.states = new SelectList(entity.tblStates.Where(x => x.country_id.ToString() == con.country ), "state_id", "state");
+            return View();
+        }
+    }
+}
